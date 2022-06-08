2f550766d79de125c0b735ccd071fbf2b4af7a7a
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-06-08 19:23:14 -0400

    Implementando funcionalidad del registro

---
+++ app/src/main/java/com/example/p1_ap2_stephany_2018_0654/ui/components/prestamo/RegistroPrestamoScreen.kt
@@ -0,0 +1,79 @@
+package com.example.p1_ap2_stephany_2018_0654.ui.components.prestamo
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.example.p1_ap2_stephany_2018_0654.ui.components.prestamo.PrestamoViewModel
+
+@Composable
+fun RegistroPrestamoScreen(navHostController: NavHostController, viewModel: PrestamoViewModel = hiltViewModel()
+ ){
+
+
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "Registro Prestamos") }) }
+    ) {
+            Column(modifier = Modifier.padding(8.dp)) {
+
+                OutlinedTextField(
+                    value = viewModel.deudor,
+                    onValueChange = {viewModel.deudor = it},
+                    modifier = Modifier.fillMaxWidth(),
+                    label = {
+                        Text(text = "Deudor")
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = null)
+                    }
+                )
+                OutlinedTextField(
+                    value = viewModel.concepto,
+                    onValueChange = {viewModel.concepto = it},
+                    modifier = Modifier.fillMaxWidth(),
+                    label = {
+                        Text(text = "Concepto")
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = null)
+                    }
+                )
+
+                OutlinedTextField(
+                    label = { Text("Monto:") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = null) },
+                    value = viewModel.monto,
+                    onValueChange = { viewModel.monto = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Button(
+                    onClick = {
+                        viewModel.Guardar()
+                        navHostController.navigateUp()
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 20.dp)
+                ) {
+                    Text("Guardar ")
+
+                }
+            }
+    }
+}
