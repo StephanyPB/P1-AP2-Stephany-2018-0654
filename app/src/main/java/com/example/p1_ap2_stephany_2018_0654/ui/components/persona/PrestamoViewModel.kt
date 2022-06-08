0dae0f97ded6a4fd9bd486e6709a8825f766e080
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-06-08 18:50:25 -0400

    Implementando funcionalidad del viewModel

---
+++ app/src/main/java/com/example/p1_ap2_stephany_2018_0654/ui/components/persona/PrestamoViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.p1_ap2_stephany_2018_0654.ui.components.persona
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.p1_ap2_stephany_2018_0654.data.repository.PrestamosRepository
+import com.example.p1_ap2_stephany_2018_0654.model.Prestamo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PrestamoViewModel @Inject constructor(
+    val prestamosRepository: PrestamosRepository
+): ViewModel() {
+    var parcial = prestamosRepository.getList()
+    private set
+
+    var deudor by mutableStateOf("")
+    var concepto by mutableStateOf("")
+    var monto by mutableStateOf("")
+
+    fun Guardar(){
+        viewModelScope.launch {
+            prestamosRepository.insertar(
+                Prestamo(
+                deudor = deudor,
+                    concepto = concepto,
+                    monto = monto.toString().toFloat()
+                )
+            )
+        }
+    }
+
+}
+
