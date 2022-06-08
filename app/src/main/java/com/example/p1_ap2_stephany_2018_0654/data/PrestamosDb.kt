b9a0064ba57f496c0db4de83c04a0d836a637df0
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-06-08 18:48:34 -0400

    Cambiando nombre de la BD

--- app/src/main/java/com/example/p1_ap2_stephany_2018_0654/data/PrestamosDb.kt
+++ app/src/main/java/com/example/p1_ap2_stephany_2018_0654/data/PrestamosDb.kt
@@ -2,15 +2,15 @@ package com.example.p1_ap2_stephany_2018_0654.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.p1_ap2_stephany_2018_0654.model.Parcial1
+import com.example.p1_ap2_stephany_2018_0654.model.Prestamo
 
 @Database(
     entities = [
-        Parcial1::class
+        Prestamo::class
     ],
     exportSchema = false,
     version = 1
 )
-abstract class Parcial1Db : RoomDatabase() {
-    abstract val parcial1Dao: Parcial1Dao
+abstract class PrestamosDb : RoomDatabase() {
+    abstract val prestamoDao: PrestamoDao
 }
