7d8fb474e71152800c2db3c25337ac875aefbd39
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-06-08 18:45:50 -0400

    Cambiando nombre

--- app/src/main/java/com/example/p1_ap2_stephany_2018_0654/di/AppModule.kt
+++ app/src/main/java/com/example/p1_ap2_stephany_2018_0654/di/AppModule.kt
@@ -2,9 +2,9 @@ package com.example.p1_ap2_stephany_2018_0654.di
 
 import android.content.Context
 import androidx.room.Room
-import com.example.p1_ap2_stephany_2018_0654.data.Parcial1Dao
-import com.example.p1_ap2_stephany_2018_0654.data.repository.Parcial1Repository
-import com.example.p1_ap2_stephany_2018_0654.data.Parcial1Db
+import com.example.p1_ap2_stephany_2018_0654.data.PrestamoDao
+import com.example.p1_ap2_stephany_2018_0654.data.repository.PrestamosRepository
+import com.example.p1_ap2_stephany_2018_0654.data.PrestamosDb
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,22 +18,22 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesParcialDb(@ApplicationContext context: Context): Parcial1Db {
+    fun providesPrestamosDb(@ApplicationContext context: Context): PrestamosDb {
         return  Room.databaseBuilder(
             context,
-            Parcial1Db::class.java,
-            "ParcialDb")
+            PrestamosDb::class.java,
+            "PrestamosDb")
             .fallbackToDestructiveMigration()
             .build()
     }
 
     @Provides
-    fun providesParcial1Dao(parcial1Db: Parcial1Db): Parcial1Dao {
-        return  parcial1Db.parcial1Dao    }
+    fun providesPrestamosDao(prestamosDb: PrestamosDb): PrestamoDao {
+        return  prestamosDb.prestamoDao    }
 
     @Provides
-    fun providesParcial1Repository(parcial1Dao: Parcial1Dao): Parcial1Repository {
-        return  Parcial1Repository(parcial1Dao)
+    fun providesPrestamosRepository(prestamoDao: PrestamoDao): PrestamosRepository {
+        return  PrestamosRepository(prestamoDao)
     }
 
 
