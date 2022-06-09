29074a960131b92f87c6826cfbaa850a79c87840
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-06-08 20:29:01 -0400

    Validacion

--- app/src/main/java/com/example/p1_ap2_stephany_2018_0654/ui/components/prestamo/RegistroPrestamoScreen.kt
+++ app/src/main/java/com/example/p1_ap2_stephany_2018_0654/ui/components/prestamo/RegistroPrestamoScreen.kt
@@ -1,13 +1,16 @@
 package com.example.p1_ap2_stephany_2018_0654.ui.components.prestamo
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -17,7 +20,10 @@ import com.example.p1_ap2_stephany_2018_0654.ui.components.prestamo.PrestamoView
 fun RegistroPrestamoScreen(navHostController: NavHostController, viewModel: PrestamoViewModel = hiltViewModel()
  ){
 
-
+    var valDeudor by rememberSaveable { mutableStateOf(false) }
+    var valConcepto by rememberSaveable { mutableStateOf(false) }
+    var valMonto by rememberSaveable { mutableStateOf(false) }
+    val context = LocalContext.current
     Scaffold(
         topBar = { TopAppBar(title = { Text(text = "Registro Prestamos") }) }
     ) {
@@ -63,8 +69,18 @@ fun RegistroPrestamoScreen(navHostController: NavHostController, viewModel: Pres
 
                 Button(
                     onClick = {
-                        viewModel.Guardar()
-                        navHostController.navigateUp()
+                        valDeudor = viewModel.deudor.isBlank()
+                        valConcepto = viewModel.deudor.isBlank()
+                        valMonto = viewModel.deudor.isBlank()
+                        if(!valDeudor && !valConcepto && !valMonto){
+                            viewModel.Guardar()
+                            navHostController.navigateUp()
+                        }
+                        else{
+                            Toast.makeText(context, "Transacion fallida", Toast.LENGTH_SHORT).show()
+                        }
+
+
                     },
                     modifier = Modifier
                         .fillMaxWidth()
